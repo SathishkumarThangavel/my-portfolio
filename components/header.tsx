8502ca6ac97c10a8286ce071bf6822e45dab7e3d
Sathishkumar Thangavel <19513411+SathishkumarThangavel@users.noreply.github.com>
2023-05-09 10:58:47 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -16,7 +16,7 @@ class Header extends React.Component {
                     <h2 className="profile-name">Sathishkumar Thangavel</h2>
                     <h3>Full Stack Developer</h3>
                     <h3><a href="https://www.credly.com/badges/c208bf0a-684e-423d-b4f5-4cc6b2e2e15a/public_url" target="_blank" rel="noopener noreferrer">Certified Kubernetes Application Developer</a></h3>
-                    <h3><a href="https://hpe.com" style={{Color: "#01a982"}} target="_blank" rel="noopener noreferrer">HP Enterprise</a></h3>
+                    <h3><a href="https://hpe.com" style={{color: "#01a982"}} target="_blank" rel="noopener noreferrer">HP Enterprise</a></h3>
                     </div>
                 </div>
             </div>   
